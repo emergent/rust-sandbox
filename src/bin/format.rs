6d6e5da4b5ecd38d291b75d2868be7f6f7d5dba9
emergent <yosikawa@gmail.com>
2019-10-20 20:51:49 +0900

    add format test

---
+++ src/bin/format.rs
@@ -0,0 +1,7 @@
+const CONST_FORMAT: &'static str = "hoge:{}:hoge";
+
+fn main() {
+    //let a = format!(CONST_FORMAT, "fuga");
+    let a = format!("hoho:{}:hoho", "fuga");
+    println!("{}", a);
+}
